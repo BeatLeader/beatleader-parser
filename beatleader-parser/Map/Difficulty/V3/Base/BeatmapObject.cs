efe1ceaa2fd18b9de6874b018a8b09b9cf887d21
NSGolova <golova@golova.dev>
2024-09-07 23:50:58 +0100

    Made BeatMapObject comparable

--- beatleader-parser/Map/Difficulty/V3/Base/BeatmapObject.cs
+++ beatleader-parser/Map/Difficulty/V3/Base/BeatmapObject.cs
@@ -1,8 +1,9 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace Parser.Map.Difficulty.V3.Base
 {
-    public class BeatmapObject
+    public class BeatmapObject : IComparable<BeatmapObject>
     {
         [JsonProperty(PropertyName = "b")]
         public float Beats { get; set; }
@@ -14,5 +15,6 @@ namespace Parser.Map.Difficulty.V3.Base
         public override bool Equals(object obj) => obj is BeatmapObject other && Beats == other.Beats;
 
         public override int GetHashCode() => 650526578 + Beats.GetHashCode();
+        int IComparable<BeatmapObject>.CompareTo(BeatmapObject other) => this.Beats.CompareTo(other.Beats);
     }
 }
